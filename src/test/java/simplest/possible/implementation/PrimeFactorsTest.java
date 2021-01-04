3a33f624f4f4206d8377ea25b274c56b7485b56d
J S Sunil <sunil@Js-MacBook-Pro.local>
2021-01-04 12:29:14 +0530

    first commit

---
+++ src/test/java/simplest/possible/implementation/PrimeFactorsTest.java
@@ -0,0 +1,14 @@
+package simplest.possible.implementation;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static java.util.Collections.emptyList;
+
+public class PrimeFactorsTest {
+    @Test
+    void thereAreNoPrimeFactorsFor1() {
+
+        Assertions.assertEquals(emptyList(), new Library().someLibraryMethod(1));
+    }
+}
